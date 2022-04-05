a6ff542c3421d91100949f9427fd3e6b50ce9c4b
Alexander Eichhorn <a.eichhorn@losjet.com>
2022-04-06 00:22:15 +0200

    Initial Commit

---
+++ RealmAsyncOpenCrashDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RealmAsyncOpenCrashDemo
+//
+//  Created by Alexander Eichhorn on 06.04.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RealmAsyncOpenCrashDemo/RealmAsyncOpenCrashDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmAsyncOpenCrashDemoApp.swift
+//  RealmAsyncOpenCrashDemo
+//
+//  Created by Alexander Eichhorn on 06.04.22.
+//
+
+import SwiftUI
+
+@main
+struct RealmAsyncOpenCrashDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
